c10012e4fb455623b7d5eef02b11901361af6f44
Sabbir Ahmmed <ahmmedsabbirbd@gmail.com>
2023-06-22 10:02:51 +0600

    migration complete

---
+++ database/migrations/2023_06_22_032258_create_socials_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('socials', function (Blueprint $table) {
+            $table->id();
+            $table->string('twitterLink', 300);
+            $table->string('githubLink', 300);
+            $table->string('linkedinLink', 300);
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('socials');
+    }
+};
